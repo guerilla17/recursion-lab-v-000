95b7da0e7696944affd2c0dcbdd88c5f0cdcbc28
Gigi M <mmgamao1@gmail.com>
2019-07-14 20:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 // Code your solution here!
 
+mystring = "hello";
+
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+printString("supercalifragilisticexpialidocious");
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+
+ 
+function isPalindrome(str) {
+  return true;
+}
+isPalindrome("hello"); 
